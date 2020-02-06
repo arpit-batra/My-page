e2668a19d12194bea5c7cd68e7a7dc4433eebdca
Appy <arpitbatra98@gmail.com>
2020-02-06 17:53:05 +0530

    delete item and complete or uncomplete an item functionality added

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// var data = {
+// 	todo:[],
+// 	complete:[]
+// };
+
 document.getElementById("addElement").addEventListener('click',function () {
 	var text = prompt("What do you want to do???");
-	// if(text=="" || text==NULL){
-	// 	alert("You added Nothing :(");
-	// }
-	// else{
+	if(text){
 		var list=document.getElementById('list');
 
 
@@ -12,13 +14,40 @@ document.getElementById("addElement").addEventListener('click',function () {
 
 		var deletee = document.createElement('button');
 		deletee.classList.add('delete');
-	
+		deletee.addEventListener('click',removeItem);
+
 		var done = document.createElement('button');
 		done.classList.add('done');
+		done.addEventListener('click',itemComplete);
 
 		item.appendChild(deletee);
 		item.appendChild(done);
 
-		list.appendChild(item);
-	// }
+		list.insertBefore(item,list.childNodes[0]);
+	}
+	else{
+		alert("You added Nothing :(");
+	}
 })
+
+
+function removeItem(){
+	var item = this.parentNode;
+	var list = item.parentNode;
+	list.removeChild(item);
+}
+
+function itemComplete(){
+	var item = this.parentNode;
+	var list = item.parentNode;
+	list.removeChild(item);
+	if(list.id=="doneList"){
+		var todoList=document.getElementById("list");
+		todoList.insertBefore(item,todoList.childNodes[0]);
+	}
+	else{
+		var completedList=document.getElementById("doneList");
+		completedList.appendChild(item);
+	}
+
+}
